2da0f51624c2bd56f0fe4654fd7479695bcd18b7
Шмыгля Владислав Вячеславович <vshmyglya@mail.ru>
2025-12-12 19:14:19 +0300

    modified:   calculator_data.json 	modified:   core/interpreter/interpreter.go 	modified:   main.go 	modified:   static/index.html

--- go.mod
+++ go.mod
@@ -3,10 +3,6 @@ module app
 go 1.21
 
 require (
-	github.com/golang-jwt/jwt/v5 v5.3.0
-	github.com/google/uuid v1.6.0
-	github.com/gorilla/websocket v1.5.3
 	github.com/joho/godotenv v1.5.1
-	github.com/rs/cors v1.11.1
 	github.com/skratchdot/open-golang v0.0.0-20200116055534-eef842397966
 )
--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"time"
 
 	"app/core/interpreter"
-	"app/core/webrtc"
 	"app/ui"
 
 	"github.com/skratchdot/open-golang/open"
@@ -16,26 +15,11 @@ func main() {
 	// Инициализация интерпретатора
 	i := interpreter.NewInterpreter()
 
-	// Инициализация WebRTC сервера
-	webrtcServer := webrtc.NewServer(":8000")
-
-	// Устанавливаем ссылку на WebRTC сервер в интерпретаторе
-	i.SetWebRTCServer(webrtcServer)
-
-	// Инициализация веб-интерфейса калькулятора
 	web := ui.NewWebInterface(i)
 
 	calcAddr := ":8080"
 	calcURL := "http://localhost" + calcAddr
 
-	// Запускаем WebRTC сервер в отдельной горутине
-	go func() {
-		if err := webrtcServer.Start(); err != nil {
-			log.Fatalf("❌ WebRTC Server error: %v", err)
-		}
-	}()
-
-	// Открываем браузер с калькулятором
 	go func() {
 		time.Sleep(500 * time.Millisecond)
 		err := open.Run(calcURL)
@@ -45,10 +29,8 @@ func main() {
 	}()
 
 	fmt.Printf("🌐 Калькулятор открывается в браузере: %s\n", calcURL)
-	fmt.Printf("📞 WebRTC доступен по адресу: http://localhost:8000/webrtc/\n")
 	fmt.Println("Нажмите Ctrl+C для выхода.")
 
-	// Запускаем веб-сервер калькулятора (блокирующий вызов)
 	err := web.Start(calcAddr)
 	if err != nil {
 		log.Fatal(err)
